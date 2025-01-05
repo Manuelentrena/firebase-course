4ef7fa4a7704be4994cea277f2c9d2b8b410bf21
EntrenaDev <manuel.entrena@gmail.com>
2025-01-05 16:00:41 +0100

    test: :wrench: Added local database enviroment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,6 +4,7 @@
 
 export const environment = {
   production: false,
+  useEmulators: true,
   firebase: {
     apiKey: "AIzaSyBBIoUVukid8r6s2ROsUPUqXkery7EhUTY",
     authDomain: "fir-course-874aa.firebaseapp.com",
