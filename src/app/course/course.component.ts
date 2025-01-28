38dd58be684f8b81db249ec544ffd3cc957cc51a
EntrenaDev <manuel.entrena@gmail.com>
2025-01-28 20:42:36 +0100

    feat: :sparkles: Implemented lesson loading with pagination in CourseComponent

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -4,6 +4,7 @@ import {Course} from '../model/course';
 import {finalize, tap} from 'rxjs/operators';
 import {Observable} from 'rxjs';
 import {Lesson} from '../model/lesson';
+import { CoursesService } from '../services/courses.service';
 
 
 @Component({
@@ -14,12 +15,43 @@ import {Lesson} from '../model/lesson';
 export class CourseComponent implements OnInit {
   public loading = false;
   public course: Course;
+  public lessons: Lesson[] = [];
+  public currentPage: number = 0;
+  public orderLessons: "asc" | "desc" = "asc";
 
   public displayedColumns = ["seqNo", "description", "duration"];
 
-  constructor(private route: ActivatedRoute) {}
+  constructor(
+    private route: ActivatedRoute,
+    private coursesService: CoursesService
+  ) {}
 
   ngOnInit() {
     this.course = this.route.snapshot.data["course"];
+    this.loadLessons();
+  }
+
+  private loadLessons(): void {
+    this.loading = true;
+    this.coursesService
+      .findLessons(this.course.id, this.orderLessons, this.currentPage)
+      .pipe(
+        finalize(() => {
+          this.loading = false;
+        })
+      )
+      .subscribe({
+        next: (newLessons: Lesson[]) => {
+          this.lessons = this.lessons.concat(newLessons);
+        },
+        error: (err) => {
+          console.error("Error loading lessons:", err);
+        },
+      });
+  }
+
+  public nextPage() {
+    this.currentPage += 1;
+    this.loadLessons();
   }
 }
