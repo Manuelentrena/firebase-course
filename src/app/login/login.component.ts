02145beb05153481e6f38953d19d5c3ee1fb0aaf
EntrenaDev <manuel.entrena@gmail.com>
2025-01-30 19:56:17 +0100

    feat: :sparkles: Implemented Firebase authentication in LoginComponent with success callback

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,31 +1,47 @@
-import {Component, NgZone, OnDestroy, OnInit} from '@angular/core';
-import * as firebaseui from 'firebaseui';
-import {AngularFireAuth} from '@angular/fire/auth';
-import {Router} from '@angular/router';
-import firebase from 'firebase/app';
+import { Component, NgZone, OnDestroy, OnInit } from "@angular/core";
+import * as firebaseui from "firebaseui";
+import { AngularFireAuth } from "@angular/fire/auth";
+import { Router } from "@angular/router";
+import firebase from "firebase/app";
 import EmailAuthProvider = firebase.auth.EmailAuthProvider;
 import GoogleAuthProvider = firebase.auth.GoogleAuthProvider;
 
-
 @Component({
-    selector: 'login',
-    templateUrl: './login.component.html',
-    styleUrls: ['./login.component.scss']
+  selector: "login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.scss"],
 })
 export class LoginComponent implements OnInit, OnDestroy {
+  public ui: firebaseui.auth.AuthUI;
 
+  constructor(
+    private afAuth: AngularFireAuth,
+    private router: Router,
+  ) {}
 
-    constructor() {
+  ngOnInit() {
+    this.afAuth.app.then((app) => {
+      const uiConfig = {
+        signInOptions: [
+          EmailAuthProvider.PROVIDER_ID,
+          GoogleAuthProvider.PROVIDER_ID,
+        ],
+        callbacks: {
+          signInSuccessWithAuthResult: this.onLoginSuccessfull.bind(this),
+        },
+      };
 
-    }
+      this.ui = new firebaseui.auth.AuthUI(app.auth());
+      this.ui.start("#firebaseui-auth-container", uiConfig);
+      this.ui.disableAutoSignIn();
+    });
+  }
 
-    ngOnInit() {
+  ngOnDestroy() {
+    this.ui.delete();
+  }
 
-    }
-
-    ngOnDestroy() {
-
-    }
+  public onLoginSuccessfull(result) {
+    this.router.navigateByUrl("/courses")
+  }
 }
-
-
